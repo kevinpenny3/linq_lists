5dfeaadddd7b8adffc6e7f36f545aaa63bf967db
kevinpenny3 <kevin.penny3@yahoo.com>
2020-03-06 15:38:27 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits
+            where fruit.StartsWith("L")
+            select fruit;
+
+            foreach (var fruit in LFruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
